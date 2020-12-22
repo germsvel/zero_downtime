16d6723d564cac90f1ecf3d26c6e1c50c40f4607
German Velasco <germsvel@gmail.com>
2020-12-22 12:15:48 -0500

    Migration step 1: write to both columns
    We add the new full_name column and field, and we write to both of them.
    This allows us to have multiple versions of the app running and we can
    run the migration safely.

--- lib/zero_downtime/accounts/user.ex
+++ lib/zero_downtime/accounts/user.ex
@@ -4,6 +4,7 @@ defmodule ZeroDowntime.Accounts.User do
 
   schema "users" do
     field :name, :string
+    field :full_name, :string
 
     timestamps()
   end
@@ -13,5 +14,17 @@ defmodule ZeroDowntime.Accounts.User do
     user
     |> cast(attrs, [:name])
     |> validate_required([:name])
+    |> insert_full_name()
+  end
+
+  defp insert_full_name(changeset) do
+    if changeset.valid? do
+      name = get_change(changeset, :name)
+
+      changeset
+      |> put_change(:full_name, name)
+    else
+      changeset
+    end
   end
 end

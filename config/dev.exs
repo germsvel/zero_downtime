1fd31c4563792516cda5030541b8588e624a761c
German Velasco <germsvel@gmail.com>
2020-12-22 11:37:06 -0500

    Allow setting PORT for phoenix app

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :zero_downtime, ZeroDowntime.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :zero_downtime, ZeroDowntimeWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
